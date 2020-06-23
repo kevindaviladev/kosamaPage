3072da27809d407cec8af9f76759235e2f148526
kevin <contact@kevin.com>
2020-06-22 23:36:32 -0500

    Uno

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('btnMenu');
+var nav = document.getElementById('nav');
+
+btnMenu.addEventListener('click',function(){
+    nav.classList.toggle('mostrar');
+})
