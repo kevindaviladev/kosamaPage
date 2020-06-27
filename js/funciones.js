b9223192bf88dba1bf137b3a9fa15685ba4fe9db
kevin <contact@kevin.com>
2020-06-27 02:16:58 -0500

    Cuatro, ya todo puesto, ANTES de prueba de nueva navbar

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,12 @@
 var btnMenu = document.getElementById('btnMenu');
+const menuUL = document.querySelector(".menu");
+
 var nav = document.getElementById('nav');
+
+// var menuUL = document.getElementById('menuUL');
+
 
 btnMenu.addEventListener('click',function(){
     nav.classList.toggle('mostrar');
+    // menuUL.classList.toggle('open');
 })
