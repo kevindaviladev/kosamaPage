e0970eee65d9cfa1c72b61152490ba816b324ae0
kevin <contact@kevin.com>
2020-07-25 18:44:40 -0500

    Antes de empezar con la programacion

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,21 @@
 var btnMenu = document.getElementById('btnMenu');
 const menuUL = document.querySelector(".menu");
+const contenedor = document.querySelector(".contenedor");
+const links = document.querySelectorAll(".menu li");
+const hamburger = document.querySelector(".hamburger");
+
 
 var nav = document.getElementById('nav');
 
 // var menuUL = document.getElementById('menuUL');
 
 
-btnMenu.addEventListener('click',function(){
-    nav.classList.toggle('mostrar');
-    // menuUL.classList.toggle('open');
+hamburger.addEventListener('click',function(){
+    // nav.classList.toggle('mostrar');
+    menuUL.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+    // contenedor.classList.toggle('open');
+    hamburger.classList.toggle("toggle");
 })
